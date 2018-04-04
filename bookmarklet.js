af1cc8bed968992144ac06989dc8cb3d30d6202d
n693322 <ron.troyer@gmail.com>
2018-04-03 21:29:51 -0400

    Initial alpha commit for 2.0.0; yay refactoring.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,42 @@
-javascript:(function() {window.bookmarkver = '1.2'; if (document.location.hostname.split('.').slice(-2).join('.') == 'reddit.com' && typeof $ !== 'undefined') {$.ajax({url: 'https://raw.githubusercontent.com/j0be/PowerDeleteSuite/master/powerdeletesuite.js?'+(new Date().getDate())}).then(function(data) {var str = '<script id="pd-script">' + data + '</script>'; if ($('#pd-script').length === 0) {$('head').append(str); } else {$('#pd-script').replaceWith(str); } }, function() {alert('Error retreiving PowerDeleteSuite from github'); }); } else {if(confirm('This script is designed to be run from your user profile on reddit. Would you like to go there now?')) {document.location = 'http://reddit.com/u/me/overview'; } } })();
+javascript: (function () {
+    window.pq = function (str) {
+        return document.querySelectorAll(str);
+    };
+    window.alpha = true;
+    window._pd = {
+        bookmarkver: '1.3',
+        domain: document.location.hostname.split('.').slice(-2).join('.'),
+        baseUrl: 'https://raw.githubusercontent.com/j0be/PowerDeleteSuite/' + (alpha ? 'alpha/' : 'master/'),
+    };
+    window.xhr = function (url, cb, err) {
+        var this_xhr = new XMLHttpRequest();
+        url += (url.match(/\?/) ? '&' : '?') + (new Date()).getDate();
+        this_xhr.open('GET', url);
+        this_xhr.send(null);
+        this_xhr.onreadystatechange = function () {
+            if (this_xhr.readyState === 4) {
+                if (this_xhr.status === 200) {
+                    cb(this_xhr);
+                } else {
+                    err(this_xhr);
+                };
+            };
+        };
+        return this_xhr;
+    };
+
+    if (_pd.domain === 'reddit.com') {
+        xhr(_pd.baseUrl + 'app.js', function (response) {
+            var pd = document.createElement('script');
+            pd.setAttribute('id', 'pd-script');
+            pd.innerHTML = response.responseText;
+            pq('head')[0].appendChild(pd);
+        }, function (data) {
+            alert('Error retreiving PowerDeleteSuite from github');
+        });
+    } else {
+        if (confirm('This script is designed to be run from your user profile on reddit. Would you like to go there now?')) {
+            document.location = 'http://reddit.com/u/me/overview';
+        };
+    };
+})();
