7eb7845a7dbda5ccb1c4f6533ddebffd9784ecc6
N693322 <ron.troyer@gmail.com>
2018-11-03 13:44:07 -0400

    Array from

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ var app = {
         }
     },
     populateSubreddits: function () {
-        var subreddits = pq('#per-sr-karma tbody th').map(function (element) { return element.textContent; }),
+        var subreddits = Array.from(pq('#per-sr-karma tbody th')).map(function (element) { return element.textContent; }),
             template = pq('.pd .subreddits .template')[0].outerHTML;
 
         subreddits.forEach(function (subreddit) {
