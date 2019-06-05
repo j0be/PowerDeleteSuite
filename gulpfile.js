3bdd241d2bb38df0a7733654d1b00e63a6ebdfe1
Ronald Troyer <ron.troyer@gmail.com>
2019-06-05 12:48:16 -0400

    Reworking the markup again and using sass!

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('sass', function () {
+    return gulp.src('app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('sass:watch', function () {
+    return gulp.watch('app.scss', gulp.parallel('sass'));
+});
+
+gulp.task('default', gulp.parallel('sass:watch'));
