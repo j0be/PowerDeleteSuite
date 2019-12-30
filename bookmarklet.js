49d0c8691e97d4324c81782c79e147e2d1a7644b
Ronald Troyer <ron.troyer@gmail.com>
2019-12-30 16:58:21 -0500

    Remove extra debugger statement

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,7 +2,6 @@ javascript: (function() {
     window.bookmarkver = '1.4';
     var isReddit = document.location.hostname.split('.').slice(-2).join('.') === 'reddit.com';
     var isOverview = document.location.href.match(/\/overview\b/);
-    debugger;
     if (isReddit && isOverview) {
         var cachBustUrl = 'https://raw.githubusercontent.com/j0be/PowerDeleteSuite/master/powerdeletesuite.js?' + (new Date().getDate());
         fetch(cachBustUrl).then(function(response) {
