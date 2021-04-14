43349494411bb496c297f81fd247473b903b8d6f
Troyer, Ronald <ron.troyer@gmail.com>
2021-04-14 13:37:21 -0400

    Let's start over... again

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,31 +1,15 @@
 javascript: (function() {
-    window.bookmarkver = '2.0';
-
     window.debugging = true;
-    var branch = 'alpha/';
+    let branch = 'alpha/';
+
+    window.bookmarkver = '2.0';
     window.baseUrl = window.debugging ?
         'http://127.0.0.1:8080/' :
         'https://raw.githubusercontent.com/j0be/PowerDeleteSuite/' + branch;
 
     var isReddit = document.location.hostname.split('.').slice(-2).join('.').toLowerCase() === 'reddit.com';
     if (isReddit) {
-        var cachBustUrl = baseUrl + 'js/app.js?' + (new Date().getDate());
-        fetch(cachBustUrl).then(function(response) {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-            return response.text();
-        }).then(function(data) {
-            var item = document.createElement('script');
-            item.id = 'appjs';
-            item.innerHTML = data;
-            if (document.getElementById(item.id)) {
-                document.getElementById(item.id).remove();
-            }
-            document.getElementsByTagName('head')[0].appendChild(item);
-        }).catch(function() {
-            alert('Error retreiving PowerDeleteSuite from ' + (debugging ? 'local' : 'github'));
-        });
+        /*Run App*/
     } else if (confirm('This script is designed to run on reddit. Would you like to go there now?')) {
         document.location = 'https://www.reddit.com/u/me/';
     } else {
