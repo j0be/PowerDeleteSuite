ea8ee1a989d60a02212bb977a8ce690862a308dd
Troyer, Ronald <ron.troyer@gmail.com>
2021-04-14 16:22:19 -0400

    Start the material phase

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,15 +1,32 @@
 javascript: (function() {
-    window.debugging = true;
-    let branch = 'alpha/';
-
-    window.bookmarkver = '2.0';
-    window.baseUrl = window.debugging ?
-        'http://127.0.0.1:8080/' :
-        'https://raw.githubusercontent.com/j0be/PowerDeleteSuite/' + branch;
-
+    window.pds = {
+        bookmarkversion: '2.0',
+        fetch: function (url, options) {
+            return fetch(url, options).then(function(response) {
+                if (!response.ok) { throw Error(response.statusText); }
+                return response.text();
+            });
+        },
+        resource: function (type, id, data) {
+            var item = document.createElement(type);
+            item.id = id;
+            item.innerHTML = data;
+            if (document.getElementById(item.id)) {
+                document.getElementById(item.id).remove();
+            }
+            document.getElementsByTagName('head')[0].appendChild(item);
+        }
+    };
+    window.baseUrl = 'http://127.0.0.1:8080/';
+    /*window.baseUrl = 'https://raw.githubusercontent.com/j0be/PowerDeleteSuite/alpha';*/
     var isReddit = document.location.hostname.split('.').slice(-2).join('.').toLowerCase() === 'reddit.com';
     if (isReddit) {
-        /*Run App*/
+        var cachBustUrl = baseUrl + 'dist/app.js?' + (new Date().getDate());
+        pds.fetch(cachBustUrl).then(function(data) {
+            pds.resource('script', 'appjs', data);
+        }).catch(function() {
+            alert('Error retreiving PowerDeleteSuite from ' + (debugging ? 'local' : 'github'));
+        });
     } else if (confirm('This script is designed to run on reddit. Would you like to go there now?')) {
         document.location = 'https://www.reddit.com/u/me/';
     } else {
---
+++ dist/app.js
@@ -0,0 +1,35 @@
+Object.assign(window.pds, {
+    version: '2.0.0',
+    app: {
+        loadResources: function() {
+            return new Promise(function(resolve, reject) {
+                return Promise.all([
+                    pds.fetch(`${baseUrl}dist/style.css?${new Date().getDate()}`).then(pds.resource.bind({}, 'style', 'pdsstyles')),
+                    pds.fetch(`https://fonts.googleapis.com/icon?family=Material+Icons`).then(pds.resource.bind({}, 'style', 'materialicons')),
+                    pds.fetch(`${baseUrl}dist/app.html?${new Date().getDate()}`).then(pds.app.handleTemplate)
+                ]).then(resolve).catch(function() {
+                    alert('ERROR');
+                    reject();
+                });
+            });
+        },
+        handleTemplate: function(data) {
+            let isNewReddit = !document.querySelector('#siteTable');
+
+            let entryPoint;
+            if (document.querySelector('#pds')) {
+                entryPoint = document.querySelector('#pds').parentNode;
+            } else {
+                entryPoint = isNewReddit ?
+                    document.querySelector('div[tabindex="0"]').parentNode :
+                    document.querySelector('#siteTable');
+            }
+
+            entryPoint.innerHTML = data;
+            document.querySelector('#appver').innerHTML = pds.version;
+            document.querySelector('#bookmarkver').innerHTML = pds.bookmarkversion;
+        }
+    }
+});
+
+pds.app.loadResources();
