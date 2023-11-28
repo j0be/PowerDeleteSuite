400fe46de8487d4bd639543ff8d16da335f20882
Sandeep <sandeep.email@proton.me>
2023-11-28 14:07:11 +0000

    Update bookmarklet.js
    typo in url

--- bookmarklet.js
+++ bookmarklet.js
@@ -6,9 +6,7 @@ javascript: (function () {
   if (isReddit && isOverview) {
     var cachBustUrl = `?${new Date().getDate()}`;
     // var cachBustUrl = 'https://raw.githubusercontent.com/j0be/PowerDeleteSuite/master/powerdeletesuite.js?' + (new Date().getDate());
-    var cachBustUrl =
-      "hhttps://raw.githubusercontent.com/saandman/PowerDeleteSuite/master/powerdeletesuite.js?" +
-      new Date().getDate();
+    var cachBustUrl = "https://raw.githubusercontent.com/saandman/PowerDeleteSuite/master/powerdeletesuite.js?" + (new Date().getDate());
     fetch(cachBustUrl)
       .then(function (response) {
         return response.text();
